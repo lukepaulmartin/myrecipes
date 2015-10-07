36929f4ff6c3fc52143c687ebfd3199b613545c8
Luke Martin <luke.martin@leics-his.nhs.uk>
2015-10-07 16:20:22 +0100

    Create chefs resources and add authentication.

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,5 +1,7 @@
 class ChefsController < ApplicationController
-
+  before_action :set_chef, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
+  
   def index
     @chefs = Chef.paginate(page: params[:page], per_page: 3)
   end
@@ -12,6 +14,7 @@ class ChefsController < ApplicationController
     @chef = Chef.new(chef_params)
     if @chef.save
       flash[:success] = "Your account was created successfully"
+      session[:chef_id] = @chef.id
       redirect_to recipes_path
     else
       render 'new'
@@ -19,21 +22,18 @@ class ChefsController < ApplicationController
   end
   
   def edit
-    @chef = Chef.find(params[:id])
   end
   
   def update
-    @chef = Chef.find(params[:id])
     if @chef.update(chef_params)
       flash[:success]= "Your profile has been updated successfully"
-      redirect_to recipes_path #TODO: Change to chefs page
+      redirect_to chef_path(@chef)
     else
       render :edit
     end
   end
     
   def show
-    @chef = Chef.find(params[:id])
     @recipes = @chef.recipes.paginate(page: params[:page], per_page: 3)
   end
   
@@ -45,5 +45,16 @@ class ChefsController < ApplicationController
   
     def chef_params
       params.require(:chef).permit(:name, :email, :password)
+    end
+    
+    def require_same_user
+      if current_user != @chef
+        flash[:danger] = "You can only edit your own profile"
+        redirect_to root_path
+      end
+    end
+    
+    def set_chef
+      @chef = Chef.find(params[:id])
     end
 end
